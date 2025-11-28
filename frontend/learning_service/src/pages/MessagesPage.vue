d296c71e63fed62647d44a0d4ac84b33ef62f240
VitalyMenkeev <login.nomad@ya.ru>
2025-11-28 13:27:44 +0300

    feat: add messages list rendering in MessagePage.vue

--- frontend/learning_service/src/pages/MessagesPage.vue
+++ frontend/learning_service/src/pages/MessagesPage.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Messages</h1>
+  <h1 class="text-h6 text-center">Messages</h1>
+
+  <q-list separator>
+    <q-item v-ripple v-for="item in messages" :key="item.id">
+      <q-item-section>
+        <q-item-label> {{ item.message }}</q-item-label>
+        <q-item-label caption>
+          {{ new Date(item.date_time * 1000).toLocaleString("ru-RU") }}
+        </q-item-label>
+      </q-item-section>
+    </q-item>
+  </q-list>
+
 </template>
+<script setup>
+import { onMounted, ref } from 'vue'
+import { messageRepository } from 'src/repositories/messageRepository'
+
+const messages = ref([])
+
+onMounted(async () => {
+  messages.value = await messageRepository.getMessages()
+
+})
+</script>
